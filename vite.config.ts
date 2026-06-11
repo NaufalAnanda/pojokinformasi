a24ffbc2e37a137b58b236bb7330dda5cabafb43
naufalananda <naufalananda79@gmail.com>
2026-06-11 15:57:26 +0800

    clot

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
     server: { entry: "server" },
-  }, // <--- PASTIKAN ADA TANDA KOMA DI SINI
+  },
   nitro: {
     preset: "vercel",
   },
